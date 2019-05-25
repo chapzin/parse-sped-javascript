b7fbe35c0f31e594a0b335ed167094678e4eb2d2
Rafael Marques <rafa.marques.learn@gmail.com>
2019-05-25 20:19:28 -0300

    Adicionado model 'D140'

---
+++ src/app/models/RegD140.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+
+const RegSchema = new mongoose.Schema({
+  reg: {
+    type: String,
+    required: true,
+  },
+  codPartConsg: {
+    type: String,
+  },
+  codMunOrig: {
+    type: String,
+    required: true,
+  },
+  codMunDest: {
+    type: String,
+    required: true,
+  },
+  indVeic: {
+    type: String,
+    required: true,
+  },
+  veicId: {
+    type: String,
+  },
+  indNav: {
+    type: String,
+    required: true,
+  },
+  viagem: {
+    type: String,
+  },
+  vlFrtLiq: {
+    type: String,
+    required: true,
+  },
+  vlDespPort: {
+    type: String,
+  },
+  vlDespCarDest: {
+    type: String,
+  },
+  vlOut: {
+    type: String,
+  },
+  vlFrtBrt: {
+    type: String,
+    required: true,
+  },
+  vlFrtMm: {
+    type: String,
+  },
+  reg0000: {
+    type: String,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('regD140', RegSchema)
