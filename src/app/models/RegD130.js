e9d02b103f109c70c8bef0fcb34fbc8c7a4d3102
Rafael Marques <rafa.marques.learn@gmail.com>
2019-05-25 10:00:20 -0300

    Adicionado modelo 'D130'

---
+++ src/app/models/RegD130.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+
+const RegSchema = new mongoose.Schema({
+  reg: {
+    type: String,
+    required: true,
+  },
+  codPartConsg: {
+    type: String,
+  },
+  codPartRed: {
+    type: String,
+  },
+  indFrtRed: {
+    type: String,
+    required: true,
+  },
+  codMunOrig: {
+    type: String,
+    required: true,
+  },
+  codMunDest: {
+    type: String,
+    required: true,
+  },
+  veicId: {
+    type: String,
+  },
+  vlLiqFrt: {
+    type: String,
+    required: true,
+  },
+  vlSecCat: {
+    type: String,
+  },
+  vlDesp: {
+    type: String,
+  },
+  vlPedg: {
+    type: String,
+  },
+  vlOut: {
+    type: String,
+  },
+  vlFrt: {
+    type: String,
+    required: true,
+  },
+  ufId: {
+    type: String,
+  },
+  reg0000: {
+    type: String,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('regD130', RegSchema)
