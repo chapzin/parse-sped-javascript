19b981540fa5d48f19cf572f772371faea9607f8
Rafael Marques <rafa.marques.learn@gmail.com>
2019-05-27 22:02:57 -0300

    Adicionado model 'D180'

---
+++ src/app/models/RegD180.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose')
+
+const RegSchema = new mongoose.Schema({
+  reg: {
+    type: String,
+    required: true,
+  },
+  numSeq: {
+    type: String,
+    required: true,
+  },
+  indEmit: {
+    type: String,
+    required: true,
+  },
+  cnpjCpfEmit: {
+    type: String,
+    required: true,
+  },
+  ufEmit: {
+    type: String,
+    required: true,
+  },
+  ieEmit: {
+    type: String,
+  },
+  codMunOrig: {
+    type: String,
+    required: true,
+  },
+  cnpjCpfTom: {
+    type: String,
+    required: true,
+  },
+  ufTom: {
+    type: String,
+    required: true,
+  },
+  ieTom: {
+    type: String,
+  },
+  codMunDest: {
+    type: String,
+    required: true,
+  },
+  codMod: {
+    type: String,
+    required: true,
+  },
+  ser: {
+    type: String,
+    required: true,
+  },
+  sub: {
+    type: String,
+  },
+  numDoc: {
+    type: String,
+    required: true,
+  },
+  dtDoc: {
+    type: String,
+    required: true,
+  },
+  vlDoc: {
+    type: String,
+    required: true,
+  },
+  reg0000: {
+    type: String,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('regD180', RegSchema)
