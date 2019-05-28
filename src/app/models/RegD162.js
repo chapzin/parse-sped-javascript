da0165ab51523654e3bb1c1f04f7137217984f45
Rafael Marques <rafa.marques.learn@gmail.com>
2019-05-27 21:30:45 -0300

    Adicionado model 'D162'

---
+++ src/app/models/RegD162.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const RegSchema = new mongoose.Schema({
+  reg: {
+    type: String,
+    required: true,
+  },
+  codMod: {
+    type: String,
+  },
+  ser: {
+    type: String,
+  },
+  numDoc: {
+    type: String,
+    required: true,
+  },
+  dtDoc: {
+    type: String,
+  },
+  vlDoc: {
+    type: String,
+  },
+  vlMerc: {
+    type: String,
+  },
+  qtdVol: {
+    type: String,
+    required: true,
+  },
+  pesoBrt: {
+    type: String,
+  },
+  pesoLiq: {
+    type: String,
+  },
+  reg0000: {
+    type: String,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('regD162', RegSchema)
