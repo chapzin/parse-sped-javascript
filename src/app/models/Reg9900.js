38a3e9f8972b94b28a4648895e3be4f72612b716
Ricardo Gomes <chapzin@gmail.com>
2019-05-29 14:59:15 -0300

    add bloco 9

---
+++ src/app/models/Reg9900.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const RegSchema = new mongoose.Schema({
+  reg: {
+    type: String,
+    required: true,
+  },
+  regBlc: {
+    type: String,
+    required: true,
+  },
+  qtdRegBlc: {
+    type: String,
+    required: true,
+  },
+  reg0000: {
+    type: Object,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("reg9900", RegSchema);
