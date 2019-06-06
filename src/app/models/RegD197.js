a46a93f9eff993983e7c22012907de29c046f637
Rafael Marques <rafa.marques.learn@gmail.com>
2019-06-05 23:47:36 -0300

    Adicionado model 'D197'

---
+++ src/app/models/RegD197.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const RegSchema = new mongoose.Schema({
+  reg: {
+    type: String,
+    required: true
+  },
+  codAj: {
+    type: String,
+    required: true
+  },
+  descrComplAj: {
+    type: String
+  },
+  codItem: {
+    type: String
+  },
+  vlBcIcms: {
+    type: String
+  },
+  aliqIcms: {
+    type: String
+  },
+  vlIcms: {
+    type: String
+  },
+  vlOutros: {
+    type: String
+  },
+  reg0000: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("regD197", RegSchema);
