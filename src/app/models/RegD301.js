388fd9d40110d76351648459c7ebaeb6b26c8256
Rafael Marques <rafa.marques.learn@gmail.com>
2019-06-06 19:36:28 -0300

    Adicionado model 'D301'

---
+++ src/app/models/RegD301.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const RegSchema = new mongoose.Schema({
+  reg: {
+    type: String,
+    required: true
+  },
+  numDocCanc: {
+    type: String,
+    required: true
+  },
+  reg0000: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("regD301", RegSchema);
