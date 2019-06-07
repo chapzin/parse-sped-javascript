8dcae6db04584eabf399e380a05299c387e5a476
Rafael Marques <rafa.marques.learn@gmail.com>
2019-06-06 22:34:22 -0300

    Adicionado model 'D365'

---
+++ src/app/models/RegD365.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const RegSchema = new mongoose.Schema({
+  reg: {
+    type: String,
+    required: true
+  },
+  codTotPar: {
+    type: String,
+    required: true
+  },
+  vlrAcumTot: {
+    type: String,
+    required: true
+  },
+  nrTot: {
+    type: String
+  },
+  descrNrTot: {
+    type: String
+  },
+  reg0000: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("regD365", RegSchema);
