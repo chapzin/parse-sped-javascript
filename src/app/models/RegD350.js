b94fda962b6d15039024c7d06b78b6913770b453
Rafael Marques <rafa.marques.learn@gmail.com>
2019-06-06 22:28:42 -0300

    Adicionado model 'D350'

---
+++ src/app/models/RegD350.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const RegSchema = new mongoose.Schema({
+  reg: {
+    type: String,
+    required: true
+  },
+  codMod: {
+    type: String,
+    required: true
+  },
+  ecfMod: {
+    type: String,
+    required: true
+  },
+  ecfFab: {
+    type: String,
+    required: true
+  },
+  ecfFab: {
+    type: String,
+    required: true
+  },
+  ecfCx: {
+    type: String,
+    required: true
+  },
+  reg0000: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("regD350", RegSchema);
