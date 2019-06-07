721d92f43bbc88ae9bfef7f0ef5609b9ba6f42a1
Rafael Marques <rafa.marques.learn@gmail.com>
2019-06-06 22:41:05 -0300

    Adicionado model 'D390'

---
+++ src/app/models/RegD390.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const RegSchema = new mongoose.Schema({
+  reg: {
+    type: String,
+    required: true
+  },
+  cstIcms: {
+    type: String,
+    required: true
+  },
+  cfop: {
+    type: String,
+    required: true
+  },
+  aliqIcms: {
+    type: String
+  },
+  vlOpr: {
+    type: String,
+    required: true
+  },
+  vlBcIssqn: {
+    type: String
+  },
+  aliqIssqn: {
+    type: String
+  },
+  vlIssqn: {
+    type: String
+  },
+  vlBcIcms: {
+    type: String,
+    required: true
+  },
+  vlIcms: {
+    type: String,
+    required: true
+  },
+  codObs: {
+    type: String
+  },
+  reg0000: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("regD390", RegSchema);
