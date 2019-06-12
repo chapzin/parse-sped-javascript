556115d002df2ae547b2801d16db7c953951f5eb
Rafael Marques <rafa.marques.learn@gmail.com>
2019-06-12 00:38:50 -0300

    Adicionado model 'D695'

---
+++ src/app/models/RegD695.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const RegSchema = new mongoose.Schema({
+  reg: {
+    type: String,
+    required: true
+  },
+  codMod: {
+    type: String,
+    required: true
+  },
+  ser: {
+    type: String,
+    required: true
+  },
+  nroOrdIni: {
+    type: String,
+    required: true
+  },
+  nroOrdFin: {
+    type: String,
+    required: true
+  },
+  dtDocIni: {
+    type: String,
+    required: true
+  },
+  dtDocFin: {
+    type: String,
+    required: true
+  },
+  nomMest: {
+    type: String,
+    required: true
+  },
+  chvCodDig: {
+    type: String,
+    required: true
+  },
+  reg0000: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("regD695", RegSchema);
