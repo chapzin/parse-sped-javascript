50609def7d22b673f60b01d6bd322d5b2eb3b596
Rafael Marques <rafa.marques.learn@gmail.com>
2019-06-11 23:59:21 -0300

    Adicionado model 'D590'

---
+++ src/app/models/RegD590.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const RegSchema = new mongoose.Schema({
+  reg: {
+    type: String,
+    required: true
+  },
+  cstIcms: {
+    type: String,
+    required: true
+  },
+  cfop: {
+    type: String,
+    required: true
+  },
+  aliqIcms: {
+    type: String
+  },
+  vlOpr: {
+    type: String,
+    required: true
+  },
+  vlBcIcms: {
+    type: String,
+    required: true
+  },
+  vlIcms: {
+    type: String,
+    required: true
+  },
+  vlBcIcmsUf: {
+    type: String,
+    required: true
+  },
+  vlIcmsUf: {
+    type: String,
+    required: true
+  },
+  vlRedBc: {
+    type: String,
+    required: true
+  },
+  codObs: {
+    type: String
+  },
+  reg0000: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("regD590", RegSchema);
