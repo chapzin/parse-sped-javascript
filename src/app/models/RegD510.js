d51a03cb22dbadedee78aed39d090aca99770390
Rafael Marques <rafa.marques.learn@gmail.com>
2019-06-11 23:48:59 -0300

    Adicionado model 'D510'

---
+++ src/app/models/RegD510.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+
+const RegSchema = new mongoose.Schema({
+  reg: {
+    type: String,
+    required: true
+  },
+  numItem: {
+    type: String,
+    required: true
+  },
+  codItem: {
+    type: String,
+    required: true
+  },
+  codClass: {
+    type: String,
+    required: true
+  },
+  qtd: {
+    type: String,
+    required: true
+  },
+  unid: {
+    type: String,
+    required: true
+  },
+  vlItem: {
+    type: String,
+    required: true
+  },
+  vlDesc: {
+    type: String
+  },
+  cstIcms: {
+    type: String,
+    required: true
+  },
+  cfop: {
+    type: String,
+    required: true
+  },
+  vlBcIcms: {
+    type: String
+  },
+  aliqIcms: {
+    type: String
+  },
+  vlIcms: {
+    type: String
+  },
+  vlBcIcmsUf: {
+    type: String
+  },
+  vlIcmsUf: {
+    type: String
+  },
+  indRec: {
+    type: String,
+    required: true
+  },
+  codPart: {
+    type: String
+  },
+  vlPis: {
+    type: String
+  },
+  vlCofins: {
+    type: String
+  },
+  codCta: {
+    type: String
+  },
+  reg0000: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("regD510", RegSchema);
