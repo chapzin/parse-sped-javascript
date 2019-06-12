8fe5297ea1e7609aaacbeb0f7dbe9f220c3ecf21
Rafael Marques <rafa.marques.learn@gmail.com>
2019-06-11 23:36:09 -0300

    Adicionado model 'D420'

---
+++ src/app/models/RegD420.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const RegSchema = new mongoose.Schema({
+  reg: {
+    type: String,
+    required: true
+  },
+  codMunOrig: {
+    type: String,
+    required: true
+  },
+  vlServ: {
+    type: String,
+    required: true
+  },
+  vlBcIcms: {
+    type: String,
+    required: true
+  },
+  vlIcms: {
+    type: String,
+    required: true
+  },
+  reg0000: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("regD420", RegSchema);
